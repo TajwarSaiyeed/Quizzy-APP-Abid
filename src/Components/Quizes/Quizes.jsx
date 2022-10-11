1274235a9fcb8eb3e7b7ace19ea884242fb16693
TajwrSaiyeed <rjabid36@gmail.com>
2022-10-11 23:22:09 +0600

    wrong right section add

--- src/Components/Quizes/Quizes.jsx
+++ src/Components/Quizes/Quizes.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import SIngleQuiz from '../SingleQuiz/SIngleQuiz'
 
+export const worngRightCounter = createContext()
+
 const Quizes = () => {
+  const [wrong, setWrong] = useState(0)
+  const [correct, setCorrect] = useState(0)
   const quizes = useLoaderData()
-  console.log(quizes)
+
   const { questions, name } = quizes.data
   return (
     <div className="px-4 py-5 sm:px-5 md:px-10">
-      <h2 className="text-4xl font-bold text-teal-400 mb-3">Quiz of {name}</h2>
-      <div className="grid grid-cols-1 gap-2">
-        {questions.map((question, index) => (
-          <SIngleQuiz key={question.id} num={index} question={question} />
-        ))}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <div>
+          <h2 className="text-4xl font-bold text-teal-400 mb-3">
+            Quiz of {name}
+          </h2>
+          <worngRightCounter.Provider
+            value={[wrong, setWrong, correct, setCorrect]}
+          >
+            <div className="grid grid-cols-1 lg:grid-cols-1 gap-2">
+              {questions.map((question, index) => (
+                <SIngleQuiz key={question.id} num={index} question={question} />
+              ))}
+            </div>
+          </worngRightCounter.Provider>
+        </div>
+        <div>
+          <h2 className="text-4xl font-bold text-teal-400 mb-3">
+            Quiz Answer Statictis
+          </h2>
+          <h2 className="my-2 p-2 rounded-md bg-slate-400">
+            You made mistake : {wrong}
+          </h2>
+          <h2 className="my-2 p-2 rounded-md bg-slate-400">
+            Right Answer : {correct}
+          </h2>
+        </div>
       </div>
     </div>
   )
--- src/Components/SingleQuiz/SIngleQuiz.jsx
+++ src/Components/SingleQuiz/SIngleQuiz.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { worngRightCounter } from '../Quizes/Quizes'
 
 const SIngleQuiz = ({ question, num }) => {
   const { options, correctAnswer } = question
+  const [wrong, setWrong, correct, setCorrect] = useContext(worngRightCounter)
 
   const handleSelect = (e) => {
     if (e !== correctAnswer) {
       alert("Oop's wrong answer, Try Again")
+      setWrong(wrong + 1)
     } else {
       alert('Correct')
+      setCorrect(correct + 1)
     }
   }
 
@@ -18,7 +22,7 @@ const SIngleQuiz = ({ question, num }) => {
         {' : '}
         {question.question.slice(3, question.question.length - 4)}
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 my-2">
+      <div className="grid grid-cols-1 gap-2 my-2">
         {options.map((option, idx) => (
           <div key={idx}>
             <p className="text-xl bg-gradient-to-r font-medium p-3 rounded-md text-left h-full w-full flex gap-5 items-center from-cyan-600 to-teal-300">
