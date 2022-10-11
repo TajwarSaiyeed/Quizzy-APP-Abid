0dc8fe1f575cd0a18f64f85a28c36b4027dcdc13
TajwrSaiyeed <rjabid36@gmail.com>
2022-10-11 19:55:37 +0600

    quiz update and wrong and right facilitis add

--- src/Components/SingleQuiz/SIngleQuiz.jsx
+++ src/Components/SingleQuiz/SIngleQuiz.jsx
@@ -5,29 +5,28 @@ const SIngleQuiz = ({ question, num }) => {
 
   const handleSelect = (e) => {
     if (e !== correctAnswer) {
-      alert(correctAnswer)
+      alert("Oop's wrong answer, Try Again")
     } else {
-      alert(e)
+      alert('Correct')
     }
   }
 
   return (
     <div className="px-4 py-5 sm:px-5 md:px-10 bg-slate-200 rounded-md">
-      <h1 className="text-2xl">
+      <h1 className="text-2xl text-left my-3">
         Quiz {num + 1}
         {' : '}
         {question.question.slice(3, question.question.length - 4)}
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 my-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 my-2">
         {options.map((option, idx) => (
           <div key={idx}>
-            <p className="text-xl bg-gradient-to-r font-medium p-3 rounded-md text-left h-full flex gap-5 items-center from-cyan-600 to-teal-300">
+            <p className="text-xl bg-gradient-to-r font-medium p-3 rounded-md text-left h-full w-full flex gap-5 items-center from-cyan-600 to-teal-300">
               <input
                 onChange={(e) => handleSelect(e.target.value)}
                 type="radio"
                 name="q"
                 value={option}
-                id={idx}
               />
               <span> {option}</span>
             </p>
