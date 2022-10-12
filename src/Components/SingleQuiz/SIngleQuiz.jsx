d21f31c85d7444b441a1bd2ff214d06af687ecae
TajwrSaiyeed <rjabid36@gmail.com>
2022-10-12 06:51:42 +0600

    update someting

--- src/Components/SingleQuiz/SIngleQuiz.jsx
+++ src/Components/SingleQuiz/SIngleQuiz.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react'
 import { worngRightCounter } from '../Quizes/Quizes'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { AiFillEye } from 'react-icons/ai'
 
 const SIngleQuiz = ({ question, num }) => {
   const { options, correctAnswer } = question
@@ -7,36 +10,54 @@ const SIngleQuiz = ({ question, num }) => {
 
   const handleSelect = (e) => {
     if (e !== correctAnswer) {
-      alert("Oop's wrong answer, Try Again")
+      toast("Oop's wrong answer, Try Again")
       setWrong(wrong + 1)
     } else {
-      alert('Correct')
+      toast('Correct')
       setCorrect(correct + 1)
     }
   }
 
   return (
     <div className="px-4 py-5 sm:px-5 md:px-10 bg-slate-200 rounded-md">
-      <h1 className="text-2xl text-left my-3">
-        Quiz {num + 1}
-        {' : '}
-        {question.question.slice(3, question.question.length - 4)}
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl text-left flex-1">
+          Quiz {num + 1}
+          {' : '}
+          {question.question.slice(3, question.question.length - 4)}
+        </h1>
+        <div className="w-10 h-10 cursor-pointer flex-1">
+          <AiFillEye />
+        </div>
+      </div>
       <div className="grid grid-cols-1 gap-2 my-2">
         {options.map((option, idx) => (
-          <div key={idx}>
-            <p className="text-xl bg-gradient-to-r font-medium p-3 rounded-md text-left h-full w-full flex gap-5 items-center from-cyan-600 to-teal-300">
-              <input
-                onChange={(e) => handleSelect(e.target.value)}
-                type="radio"
-                name="q"
-                value={option}
-              />
-              <span> {option}</span>
-            </p>
-          </div>
+          <label
+            key={idx}
+            className=" cursor-pointer text-xl bg-gradient-to-r font-medium p-3 rounded-md text-left h-full w-full flex gap-5 items-center from-cyan-600 to-teal-300"
+          >
+            <input
+              onChange={(e) => handleSelect(e.target.value)}
+              type="radio"
+              name="q"
+              value={option}
+            />
+            <span> {option}</span>
+          </label>
         ))}
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        // hideProgressBar={false}
+        // newestOnTop={false}
+        // closeOnClick
+        // rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   )
 }
